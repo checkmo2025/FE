c025a176103ad3744df2d5a9d953423adca2fc08
shinwokkang <tlsdnr303@gmail.com>
2026-01-27 18:55:23 +0900

    feat: MyLibraryList 반응형 UI 구현

--- src/components/base-ui/MyPage/MyLibraryList.tsx
+++ src/components/base-ui/MyPage/MyLibraryList.tsx
@@ -13,7 +13,7 @@ const MyLibraryList = () => {
     );
   };
   return (
-    <div className="w-[1048px] flex flex-wrap justify-between items-center content-center gap-y-[16px]">
+    <div className="flex flex-wrap items-center content-center w-full justify-center gap-[16px] mx-auto md:w-[774px] md:justify-between md:gap-[21px] md:gap-y-[16px] lg:w-[1048px] lg:gap-x-0 lg:gap-y-[16px]">
       {DUMMY_LIBRARY_BOOKS.map((book) => (
         <MyLibraryCard
           key={book.id}
