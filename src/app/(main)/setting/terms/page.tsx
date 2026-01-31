b979f4addd0c10d1eb80f0a6d43f10c16edb9a16
shinwokkang <tlsdnr303@gmail.com>
2026-01-31 11:19:09 +0900

    세팅 테블릿 반응형 UI 구현

--- src/app/(main)/setting/terms/page.tsx
+++ src/app/(main)/setting/terms/page.tsx
@@ -5,12 +5,21 @@ import { TERMS_DATA } from "@/constants/setting/terms";
 
 export default function TermsPage() {
   return (
-    <div className="flex w-[1152px] flex-col items-start gap-[24px] pb-[73px] pl-[68px] pr-[400px]">
+    // 전체 컨테이너
+    <div
+      className="flex flex-col items-start gap-[24px] pb-[73px]
+      w-full md:w-[480px] md:px-0
+      xl:w-[1152px] xl:pl-[68px] xl:pr-[400px]"
+    >
       {/* 1. 타이틀 영역 */}
       <SettingsTitle title="이용약관" />
 
       {/* 2. 본문 영역 */}
-      <div className="flex w-[688px] flex-col items-start gap-[40px] px-[20px]">
+      <div
+        className="flex flex-col items-start gap-[40px] px-[20px]
+        w-full md:w-[480px]
+        xl:w-[688px]"
+      >
         {/* 약관 리스트 컨테이너 */}
         <div className="flex flex-col items-start gap-[24px] self-stretch">
           {TERMS_DATA.map((term, index) => (
--- src/app/(main)/setting/version/page.tsx
+++ src/app/(main)/setting/version/page.tsx
@@ -2,13 +2,23 @@ import SettingsTitle from "@/components/base-ui/Settings/SettingsTitle";
 
 export default function VersionPage() {
   return (
-    <div className="flex w-[1152px] flex-col items-start gap-[24px] pb-[314px] pl-[68px] pr-[400px]">
+    // 전체 컨테이너
+
+    <div
+      className="flex flex-col items-start gap-[24px] pb-[314px]
+      w-full md:w-[480px] md:px-0
+      xl:w-[1152px] xl:pl-[68px] xl:pr-[400px]"
+    >
       {/* 1. 타이틀 영역 */}
       <SettingsTitle title="버전 정보" />
 
       {/* 2. 본문 영역 */}
-      <div className="flex w-[688px] flex-col items-start gap-[40px] px-[20px]">
-        {/* 콘텐츠 래퍼  */}
+      <div
+        className="flex flex-col items-start gap-[40px] px-[20px]
+        w-full md:w-[480px]
+        xl:w-[688px]"
+      >
+        {/* 콘텐츠 래퍼*/}
         <div className="flex flex-col items-start gap-[24px] self-stretch">
           {/* 버전 날짜 텍스트: Body_1.3, Gray_5 */}
           <p className="body_1_3 text-Gray-5">
