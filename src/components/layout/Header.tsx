ff0d0c5502f9b86af71a19ce7410099a417a1894
bini0918 <165636716+bini0918@users.noreply.github.com>
2026-02-17 01:39:57 +0900

    style: 헤더에 양옆 여백 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   return (
     <header className="w-full bg-primary-1">
-      <div className="mx-auto w-full max-w-[1440px] px-4 py-3 t:px-6 t:py-3.5 d:px-3">
+      <div className="mx-auto w-full max-w-[1440px] px-4 py-3 t:px-6 t:py-3.5 d:px-5">
         <div className="relative flex items-center justify-between w-full">
           {/*로고 + 메뉴*/}
           <div className="flex items-center t:gap-2.5 d:gap-8">
@@ -75,7 +75,7 @@ export default function Header() {
           </span>
 
           {/*아이콘*/}
-          <div className="flex items-center gap-2.5 t:gap-4">
+          <div className="flex items-center gap-2.5 t:gap-4 d:mr-1">
             <button
               onClick={() => setIsSearchOpen(!isSearchOpen)}
               aria-label="검색"
