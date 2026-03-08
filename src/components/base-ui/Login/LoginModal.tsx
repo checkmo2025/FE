95e41e19180cc3f966873d802b7d6fdf85a93f8c
shinwookkang <tlsdnr303@gmail.com>
2026-03-08 18:45:40 +0900

    feat: 아이디 찾기 UI 퍼블리싱 (#183)

--- src/components/base-ui/Login/LoginModal.tsx
+++ src/components/base-ui/Login/LoginModal.tsx
@@ -146,7 +146,10 @@ export default function LoginModal({
 
               {/* 아이디/비번 찾기 */}
               <div className={styles.findAccount}>
-                <span className={styles.link} onClick={onFindAccount}>
+                <span className={styles.link} onClick={() => {
+                  onClose();
+                  router.push('/find-account');
+                }}>
                   아이디 찾기
                 </span>
                 <span className={styles.divider}>|</span>
